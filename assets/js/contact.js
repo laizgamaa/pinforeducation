4290c56af57c81918fbd2315f6fea719dda434e0
Laiz Lira <laizgamalira@gmail.com>
2020-04-25 14:24:41 -0300

    First website version

---
+++ assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Por favor, informe seu nome",
+                    minlength: "Seu nome dever ter ao menos 2 caracteres"
+                },
+                subject: {
+                    required: "Por favor, informe o assunto",
+                    minlength: "O assunto dever ter ao menos 4 caracteres"
+                },
+                number: {
+                    required: "Por favor, informe seu número",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Por favor, informe seu e-mail"
+                },
+                message: {
+                    required: "A mensagem não pode estar vazia",
+                    minlength: "Tem certeza de que não está faltando algo?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
